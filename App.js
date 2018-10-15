1a6a762ddf8bed0edcbd8eba5ddc090f7f13fdcc
kobayashi takuya <kobataku0629@gmail.com>
2018-10-16 06:26:14 +0900

    録音・再生

--- App.js
+++ App.js
@@ -1,28 +1,80 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from 'react-native';
+import { AudioRecorder, AudioUtils } from 'react-native-audio';
+import Sound from 'react-native-sound';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const audioPath = AudioUtils.DocumentDirectoryPath + '/test.awb';
 
 export default class App extends Component {
+  state = {
+    path: ''
+  };
+
+  componentDidMount() {
+    AudioRecorder.prepareRecordingAtPath(audioPath, {
+      SampleRate: 16000,
+      Channels: 1,
+      AudioEncoding: 'amr_wb',
+    });
+
+    AudioRecorder.onProgress = ({ currentTime }) => {
+      console.log(currentTime);
+      if (currentTime >= 5) {
+        this.stopRecording();
+      }
+    };
+  }
+
+  async startRecording() {
+    console.log('start recording');
+    return await AudioRecorder.startRecording();
+  }
+
+  async stopRecording() {
+    const path = await AudioRecorder.stopRecording();
+    console.log(path);
+    this.setState({
+      path,
+    });
+  }
+
+  play() {
+    const { path } = this.state;
+    if (path === '') return;
+    const sound = new Sound(path, Sound.DOCUMENT, (error) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      console.log('success load');
+      sound.play((success) => {
+        if (success) {
+          console.log('finished');
+        } else {
+          console.log('decode error');
+          sound.reset();
+        }
+      })
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Button
+          title="録音"
+          onPress={async () => this.startRecording()}
+        />
+        <Button
+          title="再生"
+          onPress={() => this.play()}
+        />
       </View>
     );
   }
@@ -34,15 +86,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
